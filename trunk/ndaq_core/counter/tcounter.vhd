c6e56d6e7e55f3ae2a10939569e87587f38f0dc8
rafael.g.gama <rafael.g.gama@fc431e95-3d68-e601-8af3-a3fa6075667a>
2012-07-13 22:03:50 +0000

    *Thamys Version* - Corrected bug: Synchronized timebase generator and trigger counter.

--- trunk/ndaq_core/counter/tcounter.vhd
+++ trunk/ndaq_core/counter/tcounter.vhd
@@ -19,6 +19,7 @@ entity tcounter is
 		signal clk					: in	std_logic;
 		-- Counter
 		signal trigger_in			: in	std_logic;
+		signal timebase_en			: in	std_logic;
 		signal enable				: in	std_logic;
 		signal srst					: in	std_logic;
 		--
@@ -51,9 +52,12 @@ architecture rtl of tcounter is
 	signal r_srst					: std_logic := '0';
 	signal i_counter				: TCOUNTER_DATA_T := x"00000000";
 	signal counter_reg				: TCOUNTER_DATA_T := x"00000000";
+	signal reg_wait					: std_logic := '0';
 	
 	signal fifo_full				: std_logic	:= '0';
 	signal fifo_wen					: std_logic := '0';
+	
+	signal r_fifo_wen				: std_logic := '0';
 
 --
 --
@@ -78,7 +82,7 @@ begin
 		if (r_srst = '1') then
 			i_counter	<= (others => '0');
 		
-		elsif ((enable = '1') and (trigger_in = '1')) then
+		elsif ((enable = '1') and (trigger_in = '1') and (timebase_en = '1')) then
 			i_counter	<= i_counter + 1;
 	
 		end if;			
@@ -91,12 +95,43 @@ COUNTER_REGISTER: process(rst, clk)
 begin
 	if (rst ='1') then
 		counter_reg <= (others => '0');
+		reg_wait		<= '0';
 	elsif (rising_edge(clk)) then
-		counter_reg <= i_counter;
+
+		if ((timebase_en = '0') and (reg_wait = '0')) then
+			counter_reg		<= i_counter;
+			reg_wait		<= '1';
+		end if;
+		
+		if (timebase_en = '1') then
+			reg_wait		<= '0';
+		end if;
+
 	end if;
 end process;
 
 --***********************************************************************************************
+
+--
+-- Register Copier
+REGISTER_COPIER: process(rst, clk)
+begin
+	if (rst ='1') then
+		fifo_wen	<= '0';
+		r_fifo_wen	<= '0';
+		
+	elsif (rising_edge(clk)) then
+
+		if ((timebase_en = '0') and (reg_wait = '0') and (fifo_full = '0')) then
+			fifo_wen	<= '1';
+		else
+			fifo_wen	<= '0';
+		end if;
+		
+		r_fifo_wen	<= fifo_wen;
+		
+	end if;
+end process;
 
 --
 --Readout FIFO
@@ -107,7 +142,7 @@ READOUT_FIFO : counter_fifo PORT MAP
 	rdclk	 	=> rdclk,
 	rdreq	 	=> rden,
 	wrclk	 	=> clk,
-	wrreq	 	=> '1',
+	wrreq	 	=> r_fifo_wen,
 	q	 		=> counter_q,
 	rdempty		=> fifo_empty,
 	wrfull		=> fifo_full
